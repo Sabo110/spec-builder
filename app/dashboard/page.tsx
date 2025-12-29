97aa8086d1f705cdb5b7a06c961ce68917fabdc0
sabo110 <samirphanuel@gmail.com>
2025-12-29 01:47:31 +0100

    v1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div >
+      <h1 className="text-2xl font-bold">Votre espace de travail</h1>
     </div>
   )
 }
